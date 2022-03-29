982c7fe0092fb5b081a22b7cc490e0579f5ec9a7
aka773 <alkaloid77@naver.com>
2022-03-29 19:10:32 +0900

    목업 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Styled from 'styled-components';
+
+// import Caver from 'caver-js';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+import Home from './pages/Home';
+import NFT from './pages/NFT';
+
+import Modal from './components/modals/Modal';
+
+// const caver = new Caver('http://localhost:8551/');
+
+const backgroundImg = {
+	// backgroundImage:
+	// 	"url(https://source.unsplash.com/random/1920x1080)",
+	// width: "100%",
+    // height: "100%",
+	// opacity: "0.5"
+};
 
 const App = () => {
 	return (
-		<div className="App">
+		<div className="App" style={backgroundImg}>
 			<Navbar />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='/nft' element={<NFT />} />
+			</Routes>
 			<Footer />
 		</div>
 	);
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Home = () => {
 
-	return(
+	return (
 		<>
+			<div className='grid place-items-center h-screen'>
+				<div className='m-auto'>
+					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+						Welcome to The Governance!
+					</h5>
+					<p className="font-normal text-gray-700 dark:text-gray-400">
+						The Governance는 NFT를 이용한 거버넌스로 운영되는 NFT 커뮤니티입니다. NFT를 이용해서 클럽에 가입하고 투표할 수 있습니다.
+					</p>
+				</div>
+			</div>
 		</>
 	);
 }
